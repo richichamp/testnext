22fdd1185cc011676b3dba09bcb175cdc8c5fd36
Riccardo Campioni <richichamp@gmail.com>
2018-10-30 09:58:33 +0100

    first commit

---
+++ out/_next/539fa1ec-cb3f-4605-9320-0bc934fc7faf/page/about.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([3],{195:function(e,t,n){e.exports=n(196)},196:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(9),o=n.n(u);t.default=function(){return o.a.createElement("div",null,"This is the about page.")}}},[195]).default}});
